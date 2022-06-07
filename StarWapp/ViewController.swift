48d39ece0f6cc46c7579b69e9628c80813a8b51d
Desenv - Wallace Baldenebre Da Silva <wallace.silva@bancodaycoval.com.br>
2022-06-06 21:55:53 -0300

    Initial Commit

---
+++ StarWapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWapp
+//
+//  Created by Trabalho on 06/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
